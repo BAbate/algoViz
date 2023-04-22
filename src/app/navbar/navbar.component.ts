97147bbbd675d9e3dadace854093c0201163f25c
BAbate <birukdejene2012@gmail.com>
2023-04-22 13:49:07 -0500

    docker containarize

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,20 +1,30 @@
 import { Router } from '@angular/router';
 import { AuthServiceService } from './../auth-service.service';
-import { Component } from '@angular/core';
+import { ChangeDetectorRef, Component, OnChanges, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.component.css']
 })
-export class NavbarComponent {
+export class NavbarComponent implements OnInit, OnChanges{
   
-  isLoggedIn: boolean = sessionStorage.getItem('isLoggedIn') ? true : false;
+  isLoggedIn: boolean = false;
   
-  constructor(private authService: AuthServiceService, private router: Router) {}
+  constructor(private authService: AuthServiceService, private router: Router, private cdr: ChangeDetectorRef) {}
+
+  ngOnInit(): void {
+      this.isLoggedIn = this.authService.isAuthenticated();
+      this.cdr.detectChanges();
+  }
 
   logout(): void {
     this.authService.logout();
     this.router.navigate(['/login']);
+  }
+
+  ngOnChanges(): void {
+    this.isLoggedIn = this.authService.isAuthenticated();
+    this.cdr.detectChanges();
   }
 }
